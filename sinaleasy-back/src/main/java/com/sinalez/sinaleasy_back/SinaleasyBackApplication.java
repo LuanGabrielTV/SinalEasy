aa9b71fb9a3ce19e9b0a5e730ab001997aa4de6c
Jonas <jonascamargo999@gmail.com>
2024-11-06 22:50:33 -0300

    feat: adiciona o backend spring

---
+++ sinaleasy-back/src/main/java/com/sinalez/sinaleasy_back/SinaleasyBackApplication.java
@@ -0,0 +1,13 @@
+package com.sinalez.sinaleasy_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SinaleasyBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SinaleasyBackApplication.class, args);
+	}
+
+}
---
+++ sinaleasy-back/src/test/java/com/sinalez/sinaleasy_back/SinaleasyBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sinalez.sinaleasy_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SinaleasyBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
