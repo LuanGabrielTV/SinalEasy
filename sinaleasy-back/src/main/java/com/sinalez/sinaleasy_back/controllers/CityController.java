a4a0adfe2ac0a9855cee7a2be57b14b2d5312be3
Jonas <jonascamargo999@gmail.com>
2024-11-10 10:11:03 -0300

    feat: add  city endpoint to create new city

--- sinaleasy-back/src/main/java/com/sinalez/sinaleasy_back/controllers/CityController.java
+++ sinaleasy-back/src/main/java/com/sinalez/sinaleasy_back/controllers/CityController.java
@@ -4,11 +4,16 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.sinalez.sinaleasy_back.dtos.CityRecordDTO;
 import com.sinalez.sinaleasy_back.entities.City;
 import com.sinalez.sinaleasy_back.services.CityService;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/cities")
@@ -19,11 +24,18 @@ public class CityController {
         this.cityService = cityService;
     }
 
+    @PostMapping("/")
+    public ResponseEntity<City> createCity(@RequestBody @Valid CityRecordDTO cityRecordDTO) {
+        City createdCity = cityService.createCity(cityRecordDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdCity);
+    }
+
     @GetMapping("/{id}")
     public ResponseEntity<Object> getCityById(@PathVariable(value = "id") Integer id) {
         City city = cityService.getCityById(id);
         return ResponseEntity.status(HttpStatus.OK).body(city);
     }
+
 
 
 }
--- sinaleasy-back/src/main/java/com/sinalez/sinaleasy_back/services/CityService.java
+++ sinaleasy-back/src/main/java/com/sinalez/sinaleasy_back/services/CityService.java
@@ -1,7 +1,9 @@
 package com.sinalez.sinaleasy_back.services;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
+import com.sinalez.sinaleasy_back.dtos.CityRecordDTO;
 import com.sinalez.sinaleasy_back.entities.City;
 import com.sinalez.sinaleasy_back.exceptions.customExceptions.CityNotFoundException;
 import com.sinalez.sinaleasy_back.repositories.CityRepository;
@@ -12,6 +14,12 @@ public class CityService {
 
     public CityService(CityRepository cityRepository) {
         this.cityRepository = cityRepository;
+    }
+
+    public City createCity(CityRecordDTO cityRecordDTO) {
+        City city = new City();
+        BeanUtils.copyProperties(cityRecordDTO, city);
+        return cityRepository.save(city);
     }
 
     public City getCityById(Integer id) {
