a627f050191c8228fec35aa2b556deedcad7a06b
Jonas <jonascamargo999@gmail.com>
2024-11-10 09:27:18 -0300

    fix: fix signal entity

--- sinaleasy-back/src/main/java/com/sinalez/sinaleasy_back/entities/Signal.java
+++ sinaleasy-back/src/main/java/com/sinalez/sinaleasy_back/entities/Signal.java
@@ -1,23 +1,33 @@
 package com.sinalez.sinaleasy_back.entities;
 
+import java.io.Serializable;
 import java.util.UUID;
 
 import com.sinalez.sinaleasy_back.enums.Status;
 import com.sinalez.sinaleasy_back.enums.TypeOfSignal;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
-public class Signal {
+@Entity
+@Table(name = "TB_Signal")
+@Getter
+@Setter
+public class Signal implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID idSignal;
+
     private String name;
     private String description;
-    private TypeOfSignal string;
+    private TypeOfSignal typeOfSignal;
     private Status status;
 
     @ManyToOne
