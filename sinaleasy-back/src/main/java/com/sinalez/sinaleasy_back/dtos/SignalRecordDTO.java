d9eb16cbb43aef816eaa573e09fd71564ce50742
Jonas <jonascamargo999@gmail.com>
2024-11-14 17:46:50 -0300

    fix: adequa o DTO de signal ao domain

--- sinaleasy-back/src/main/java/com/sinalez/sinaleasy_back/dtos/SignalRecordDTO.java
+++ sinaleasy-back/src/main/java/com/sinalez/sinaleasy_back/dtos/SignalRecordDTO.java
@@ -3,21 +3,21 @@ package com.sinalez.sinaleasy_back.dtos;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
-import com.sinalez.sinaleasy_back.enums.TypeOfSignal;
-
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
+// import jakarta.validation.constraints.NotBlank;
+// import jakarta.validation.constraints.NotEmpty;
+// import jakarta.validation.constraints.NotNull;
 
 public record SignalRecordDTO(
-    @NotBlank String name,
-    @NotBlank String description,
-    @NotBlank String address,
-    @NotBlank BigDecimal scaleFactor,
-    @NotNull Double latitude,
-    @NotNull Double longitude,
-    @NotEmpty TypeOfSignal typeOfSignal,
-    @NotNull Integer cityId,
-    @NotNull LocalDate date
+    String name,
+    String description,
+    String address,
+    BigDecimal scaleFactor,
+    Double latitude,
+    Double longitude,
+    Integer typeOfSignal,
+    String cityId,
+    LocalDate date,
+    Integer numberOfLikes
     
 ) {}
+
