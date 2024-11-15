60bb630bb0e41cf9b9ce3382b630c00397c6b278
Jonas <jonascamargo999@gmail.com>
2024-11-15 13:05:43 -0300

    fix: fix DTO NotNull/Blank notation

--- sinaleasy-back/src/main/java/com/sinalez/sinaleasy_back/dtos/SignalRecordDTO.java
+++ sinaleasy-back/src/main/java/com/sinalez/sinaleasy_back/dtos/SignalRecordDTO.java
@@ -3,19 +3,23 @@ package com.sinalez.sinaleasy_back.dtos;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 // import jakarta.validation.constraints.NotBlank;
 // import jakarta.validation.constraints.NotEmpty;
 // import jakarta.validation.constraints.NotNull;
 
 public record SignalRecordDTO(
-    String name,
-    String description,
-    String address,
-    BigDecimal scaleFactor,
-    Double latitude,
-    Double longitude,
-    Integer typeOfSignal,
-    String cityId,
+    
+    @NotBlank String name,
+    @NotBlank String description,
+    @NotBlank String address,
+    @NotNull BigDecimal scaleFactor,
+    @NotNull Double latitude,
+    @NotNull Double longitude,
+    @NotNull Integer typeOfSignal,
+    @NotBlank String cityId,
     LocalDate date,
     Integer numberOfLikes,
     Integer Status
