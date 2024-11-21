834872d21bd16ff1c2319c35b148b88767bd0274
Jonas <jonascamargo999@gmail.com>
2024-11-21 16:02:54 -0300

    chore: troca o @NotNull dos DTOs para entidades

--- sinaleasy-back/src/main/java/com/sinalez/sinaleasy_back/dtos/CityRecordDTO.java
+++ sinaleasy-back/src/main/java/com/sinalez/sinaleasy_back/dtos/CityRecordDTO.java
@@ -1,12 +1,8 @@
 package com.sinalez.sinaleasy_back.dtos;
 
-
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-
 public record CityRecordDTO(
-    @NotNull String cityId,
-    @NotBlank String name,
-    @NotBlank String state,
-    @NotNull  Integer rating
+    String cityId,
+    String name,
+    String state,
+    Integer rating
 ) {}
