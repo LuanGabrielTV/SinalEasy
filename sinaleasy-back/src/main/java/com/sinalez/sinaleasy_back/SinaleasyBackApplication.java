fbdb6049d99de51c50e66e3276e8fe550ead9476
Jonas <jonascamargo999@gmail.com>
2024-11-22 16:17:03 -0300

    feat: adiciona docker-compose para orquestrar os arquivos Dockerfile e suas dependências

--- sinaleasy-back/src/main/java/com/sinalez/sinaleasy_back/SinaleasyBackApplication.java
+++ sinaleasy-back/src/main/java/com/sinalez/sinaleasy_back/SinaleasyBackApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SinaleasyBackApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SinaleasyBackApplication.class, args);
 	}
--- sinaleasy-back/src/main/java/com/sinalez/sinaleasy_back/mappers/CityMapper.java
+++ sinaleasy-back/src/main/java/com/sinalez/sinaleasy_back/mappers/CityMapper.java
@@ -1,14 +1,12 @@
 package com.sinalez.sinaleasy_back.mappers;
 
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 import com.sinalez.sinaleasy_back.dtos.CityRecordDTO;
 import com.sinalez.sinaleasy_back.entities.City;
 
 @Mapper(componentModel = "spring")
 public interface CityMapper {
-    // @Mapping(target = "cityId", ignore = true)
     CityRecordDTO toDTO(City city);
 
     
