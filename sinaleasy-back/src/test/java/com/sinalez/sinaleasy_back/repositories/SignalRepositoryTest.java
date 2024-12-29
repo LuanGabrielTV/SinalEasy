ca96310cc60a2b94fa3c69f7d21d1a58bf8aea77
Jonas <jonascamargo999@gmail.com>
2024-12-29 18:58:22 -0300

    feat: cria testes para SignalRepository

--- sinaleasy-back/src/test/java/com/sinalez/sinaleasy_back/repositories/SignalRepositoryTest.java
+++ sinaleasy-back/src/test/java/com/sinalez/sinaleasy_back/repositories/SignalRepositoryTest.java
@@ -1,6 +1,6 @@
 package com.sinalez.sinaleasy_back.repositories;
 
-
+import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.DisplayName;
@@ -22,6 +22,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 @ActiveProfiles("test")
 public class SignalRepositoryTest {
 
+    // posteriormente, criar testes para achar sinais atrelados ao usuario
     @PersistenceContext
     private EntityManager entityManager;
 
@@ -32,30 +33,97 @@ public class SignalRepositoryTest {
     @DisplayName("Should get Signal successfully from DB")
     void shouldFindSignalByName() {
         // Arrange
-        City city = new City();
-        city.setCityId("1234");
-        city.setName("Name City");
-        entityManager.persist(city);
+        Signal signal = createSignal("Existent name", "123");
+        persistObjects(signal);
 
+        // Act
+        Optional<Signal> foundSignal = signalRepository.findByName("Existent name");
+    
+        // Assert
+        assertThat(foundSignal).isPresent();
+        assertThat(foundSignal.get().getName()).isEqualTo("Existent name");
+    }
+
+    @Test
+    @DisplayName("Should not get Signal successfully from DB")
+    void shouldNotFindSignalByName() {
+        // Arrange
+        Signal signal = createSignal("Existent name", "123");
+        persistObjects(signal);
+        // Act: buscando o sinal nao existente
+        Optional<Signal> foundSignal = signalRepository.findByName("Nonexistent Signal");
+
+        // Assert
+        assertThat(foundSignal).isEmpty();
+    }
+
+    @Test
+    @DisplayName("Should get Signal by CityId successfully from DB")
+    void shouldFindSignalByCityId() {
+        // Arrange
+
+        City city1 = createCity("123");
+        City city2 = createCity("000");
+
+        Signal signal1 = createSignal("Signal1");
+        signal1.setCity(city1);
+
+        Signal signal2 = createSignal("signal2");
+        signal2.setCity(city1);
+
+        Signal signal3 = createSignal("signal3");
+        signal3.setCity(city2);
+
+        persistObjects(signal1, signal2, signal3);
+
+        // Act
+        List<Signal> foundSignals = signalRepository.findByCityCityId("123");
+
+        // Assert
+        assertThat(foundSignals).hasSize(2);
+        assertThat(foundSignals).extracting(Signal::getName).containsExactlyInAnyOrder("Signal1", "signal2"); // deve conter o signal 1 e 2
+
+    }
+
+    private void persistObjects(Object... objects) {
+        for (Object object : objects) {
+            entityManager.persist(object);
+        }
+        entityManager.flush();
+    }
+
+    private City createCity(String cityId){
+        City city = new City();
+        city.setCityId(cityId);
+        city.setName("Test City");
+        entityManager.persist(city);
+        entityManager.flush();
+        return city;
+    }
+
+    private Signal createSignal(String name, String cityId) {
         Signal signal = new Signal();
-        signal.setName("Name Signal");
+        signal.setName(name);
         signal.setDescription("Test Description");
         signal.setAddress("Test Address");
         signal.setLatitude(12.345);
         signal.setLongitude(67.890);
         signal.setTypeOfSignal(1);
         signal.setStatus(1);
-        signal.setCity(city);
-        entityManager.persist(signal);
-        entityManager.flush();
-
-        // Act
-        Optional<Signal> foundSignal = signalRepository.findByName("Test Signal");
-
-        // Assert
-        assertThat(foundSignal).isPresent();
-        assertThat(foundSignal.get().getName()).isEqualTo("Test Signal");
-        assertThat(foundSignal.get().getCity().getName()).isEqualTo("Test City");
+        signal.setCity(createCity("132"));
+        return signal;
     }
-    
+
+
+    private Signal createSignal(String name) {
+        Signal signal = new Signal();
+        signal.setName(name);
+        signal.setDescription("Test Description");
+        signal.setAddress("Test Address");
+        signal.setLatitude(12.345);
+        signal.setLongitude(67.890);
+        signal.setTypeOfSignal(1);
+        signal.setStatus(1);
+        return signal;
+    }
 }
