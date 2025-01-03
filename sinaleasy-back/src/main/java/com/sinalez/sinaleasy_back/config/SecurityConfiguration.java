8e90fc672066dca7ec16d9a346c82b3186c88b9b
Jonas <jonascamargo999@gmail.com>
2025-01-03 00:41:51 -0300

    feat: cria configuração do spring security

--- sinaleasy-back/src/main/java/com/sinalez/sinaleasy_back/config/SecurityConfiguration.java
+++ sinaleasy-back/src/main/java/com/sinalez/sinaleasy_back/config/SecurityConfiguration.java
@@ -3,9 +3,13 @@ package com.sinalez.sinaleasy_back.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -17,8 +21,20 @@ public class SecurityConfiguration {
                 .csrf(csrf -> csrf.disable()) // desabilitando o Cross-Site Request Forgery, pois o client eh confiavel(frontend)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
-                .anyRequest().authenticated() // como nao temos role admin, todas as req de todos usuario precisam da auth
+                .requestMatchers(HttpMethod.POST, "/api/auth/register").permitAll() // todos tem autorizacao para tentar logar
+                .anyRequest().authenticated() // como nao temos role admin, todas as req de todos usuario precisam da auth, exceto a de 
                 )
                 .build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    // algoritmo para fazer o hash
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
