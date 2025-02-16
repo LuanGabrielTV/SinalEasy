38064f69c20f77a4428398a2de7cef9c4cd92a9b
Jonas <jonascamargo999@gmail.com>
2025-02-16 12:11:13 -0300

    refactor: remove notação autowired do AuthorizationService

--- sinaleasy-back/src/main/java/com/sinalez/sinaleasy_back/services/security/AuthorizationService.java
+++ sinaleasy-back/src/main/java/com/sinalez/sinaleasy_back/services/security/AuthorizationService.java
@@ -1,24 +1,25 @@
 package com.sinalez.sinaleasy_back.services.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import com.sinalez.sinaleasy_back.repositories.UserRepository;
 
-@Component
+@Service
 public class AuthorizationService implements UserDetailsService {
-    @Autowired
-    private  UserRepository userRepository;
+    private final UserRepository userRepository;
 
-    // public AuthorizationService(UserRepository userRepository) {
-    //     this.userRepository = userRepository;
-    // }
+    public AuthorizationService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return userRepository.findByUserLogin(username);
     }
 }
+
+
+// Toda vez que alguem tentar autenticar na nossa aplicação, o spring security vai precisar de uma forma de identificar esses usuários. Ele não sabe de onde vem esses usuários (podem vir de APIs de terceiros como o google por exemplo). A classe é uma abstração para o Spring
