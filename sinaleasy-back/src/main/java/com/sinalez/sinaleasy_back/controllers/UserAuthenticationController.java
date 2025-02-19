fef0eb8fb7129af3b46d2ee595ff945a67c1f449
Jonas <jonascamargo999@gmail.com>
2025-02-19 00:32:16 -0300

    fix: remove dependência circular

--- sinaleasy-back/src/main/java/com/sinalez/sinaleasy_back/controllers/UserAuthenticationController.java
+++ sinaleasy-back/src/main/java/com/sinalez/sinaleasy_back/controllers/UserAuthenticationController.java
@@ -2,16 +2,20 @@ package com.sinalez.sinaleasy_back.controllers;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.sinalez.sinaleasy_back.domains.User;
 import com.sinalez.sinaleasy_back.dtos.AuthenticationDTO;
 import com.sinalez.sinaleasy_back.dtos.LoginResponseDTO;
 import com.sinalez.sinaleasy_back.dtos.RegisterDTO;
 import com.sinalez.sinaleasy_back.dtos.RegisterResponseDTO;
+import com.sinalez.sinaleasy_back.infra.security.TokenService;
 import com.sinalez.sinaleasy_back.services.logic.UserService;
 
 import jakarta.validation.Valid;
@@ -21,9 +25,13 @@ import jakarta.validation.Valid;
 @CrossOrigin
 public class UserAuthenticationController {
     private final UserService userService;
+    private final TokenService tokenService;
+    private final AuthenticationManager authenticationManager;
 
-    public UserAuthenticationController(UserService userService) {
+    public UserAuthenticationController(UserService userService, TokenService tokenService, AuthenticationManager authenticationManager) {
         this.userService = userService;
+        this.tokenService = tokenService;
+        this.authenticationManager = authenticationManager;
     }
 
     @PostMapping("/register")
@@ -38,7 +46,11 @@ public class UserAuthenticationController {
 
     @PostMapping("/login")
     public ResponseEntity<LoginResponseDTO> login(@RequestBody @Valid AuthenticationDTO authenticationDTO) {
-        var token = userService.loginUser(authenticationDTO);
+        //token a ser retornado pro client
+        var usernamePassword = new UsernamePasswordAuthenticationToken(authenticationDTO.login(), authenticationDTO.password());
+        // auth manager eh configurado no securityConfiguration
+        var auth = authenticationManager.authenticate(usernamePassword);
+        var token = tokenService.generateToken((User) auth.getPrincipal());
         return ResponseEntity.status(HttpStatus.OK).body(new LoginResponseDTO(token));
     }
 
--- sinaleasy-back/src/main/java/com/sinalez/sinaleasy_back/services/logic/UserService.java
+++ sinaleasy-back/src/main/java/com/sinalez/sinaleasy_back/services/logic/UserService.java
@@ -3,30 +3,22 @@ package com.sinalez.sinaleasy_back.services.logic;
 import java.util.List;
 import java.util.UUID;
 
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.sinalez.sinaleasy_back.domains.User;
-import com.sinalez.sinaleasy_back.dtos.AuthenticationDTO;
 import com.sinalez.sinaleasy_back.dtos.RegisterDTO;
 import com.sinalez.sinaleasy_back.exceptions.customExceptions.userExceptions.UserAlreadyExists;
 import com.sinalez.sinaleasy_back.exceptions.customExceptions.userExceptions.UserEmailIsBlank;
 import com.sinalez.sinaleasy_back.exceptions.customExceptions.userExceptions.UserNotFoundException;
-import com.sinalez.sinaleasy_back.infra.security.TokenService;
 import com.sinalez.sinaleasy_back.repositories.UserRepository;
 
 @Service
 public class UserService {
     private final UserRepository userRepository;
-    private final AuthenticationManager authenticationManager;
-    private final TokenService tokenService;
 
-    public UserService(UserRepository userRepository, AuthenticationManager authenticationManager, TokenService tokenService) {
+    public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
-        this.authenticationManager = authenticationManager;
-        this.tokenService = tokenService;
     }
 
     public User registerUser(RegisterDTO registerDTO) {
@@ -44,14 +36,6 @@ public class UserService {
                 encryptedPassword, 
                 registerDTO.role());
         return userRepository.save(newUser);
-    }
-
-    public String loginUser(AuthenticationDTO authenticationDTO) {
-        //token a ser retornado pro client
-        var usernamePassword = new UsernamePasswordAuthenticationToken(authenticationDTO.login(), authenticationDTO.password());
-        // o authenticationManager eh configurado no SecurityConfiguration
-        var auth = authenticationManager.authenticate(usernamePassword);
-        return tokenService.generateToken((User) auth.getPrincipal());
     }
 
     public List<User> getUsers() {
